6a29d390e80b8dbb18e811178460a305970ac94d
Sergio Ojeda <soj3da@gmail.com>
2020-05-23 17:54:29 -0300

    Test Case 1

---
+++ tests/Feature/ProductControllerTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function test_index()
+    {
+        factory(Product::class, 5)->create();
+
+        $response = $this->getJson('/api/products');
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonCount(5, 'data');
+    }
+
+    public function test_create_new_product()
+    {
+        $data = [
+            'name' => 'Hola',
+            'price' => 1000,
+        ];
+        $response = $this->postJson('/api/products', $data);
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $this->assertDatabaseHas('products', $data);
+    }
+
+    public function test_update_product()
+    {
+        /** @var Product $product */
+        $product = factory(Product::class)->create();
+
+        $data = [
+            'name' => 'Update Product',
+            'price' => 20000,
+        ];
+
+        $response = $this->patchJson("/api/products/{$product->getKey()}", $data);
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+    }
+
+    public function test_show_product()
+    {
+        /** @var Product $product */
+        $product = factory(Product::class)->create();
+
+        $response = $this->getJson("/api/products/{$product->getKey()}");
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonCount(1);
+    }
+
+    public function test_delete_product()
+    {
+        /** @var Product $product */
+        $product = factory(Product::class)->create();
+
+        $response = $this->deleteJson("/api/products/{$product->getKey()}");
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $this->assertDeleted($product);
+    }
+
+}
