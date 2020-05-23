f4d4595b160f130d88dcb4232db26e9229113294
Sergio Ojeda <soj3da@gmail.com>
2020-05-23 19:40:25 -0300

    Editar migracion

--- database/migrations/2020_05_23_223809_create_products_table.php
+++ database/migrations/2020_05_23_223809_create_products_table.php
@@ -10,6 +10,8 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->float('price');
             $table->timestamps();
         });
     }
