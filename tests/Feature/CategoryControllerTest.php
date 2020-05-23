4419d3639038f3daafd75a5311779f667abc05a9
Sergio Ojeda <soj3da@gmail.com>
2020-05-23 20:12:44 -0300

    Endpoints de Categorias

---
+++ tests/Feature/CategoryControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Category;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CategoryControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_index()
+    {
+        factory(Category::class, 5)->create();
+
+        $response = $this->getJson('/api/categories');
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonCount(5);
+    }
+
+    public function test_create_new_category()
+    {
+        $data = [
+            'name' => 'Hola',
+        ];
+        $response = $this->postJson('/api/categories', $data);
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $this->assertDatabaseHas('categories', $data);
+    }
+
+    public function test_update_category()
+    {
+        /** @var Category $category */
+        $category = factory(Category::class)->create();
+
+        $data = [
+            'name' => 'Update Category',
+        ];
+
+        $response = $this->patchJson("/api/categories/{$category->getKey()}", $data);
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+    }
+
+    public function test_show_category()
+    {
+        /** @var Category $category */
+        $category = factory(Category::class)->create();
+
+        $response = $this->getJson("/api/categories/{$category->getKey()}");
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+    }
+
+    public function test_delete_category()
+    {
+        /** @var Category $category */
+        $category = factory(Category::class)->create();
+
+        $response = $this->deleteJson("/api/categories/{$category->getKey()}");
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $this->assertDeleted($category);
+    }
+
+}
