3ea148a2cb64144a0fc6c2d1a2be0e5c8736bd9d
Sergio Ojeda <soj3da@gmail.com>
2020-05-23 19:59:38 -0300

    Api Resource

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products', 'ProductController');
+Route::apiResource('products', 'ProductController');
