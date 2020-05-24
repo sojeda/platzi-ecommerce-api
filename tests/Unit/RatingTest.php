9484783389ae92999fdba8cba54c9bfc2ca78e3e
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 00:47:19 -0300

    Metodos Rate & Unrate

--- tests/Unit/RatingTest.php
+++ tests/Unit/RatingTest.php
@@ -13,8 +13,14 @@ class RatingTest extends TestCase
 
     public function test_a_product_belongs_to_many_users()
     {
+        /** @var User $user */
         $user = factory(User::class)->create();
         $product = factory(Product::class)->create();
+
+        $user->rate($product, 5);
+
+        // dd($user->ratings()->get());
+        // dd($product->ratings()->get())
 
         $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $user->ratings);
         $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $product->ratings);
