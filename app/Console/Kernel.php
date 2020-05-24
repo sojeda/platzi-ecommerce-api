05e0f51b6cf270b0e7385a6dc43762c0682ce649
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 17:31:49 -0300

    Clase 10 - Tareas programadas

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')->hourly();
+
+        $schedule->call(function () {
+            echo 'Test';
+        })->everyMinute();
     }
 
     /**
