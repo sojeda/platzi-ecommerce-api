61556c5bb0f6dad223db2122eb017f2da8eadfc0
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 16:37:19 -0300

    Llamar comando desde mi aplicacion.

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Console\Commands\SendEmailVerificationReminderCommand;
+use Illuminate\Support\Facades\Artisan;
+
 class NewsletterController extends Controller
 {
     public function send(): void
     {
-
+        Artisan::call(SendEmailVerificationReminderCommand::class);
     }
 }
