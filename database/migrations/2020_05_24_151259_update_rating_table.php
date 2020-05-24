de281ac75b029768f079c67f3213d07fde90908b
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 12:35:46 -0300

    Migracion de Datos

--- database/migrations/2020_05_24_151259_update_rating_table.php
+++ database/migrations/2020_05_24_151259_update_rating_table.php
@@ -20,12 +20,28 @@ class UpdateRatingTable extends Migration
             $table->morphs('rateable');
             $table->morphs('qualifier');
         });
+
+        \App\Rating::all()->each(function (\App\Rating $rating) {
+            $rating->rater_type = \App\User::class;
+            $rating->rater_id = $rating->user_id;
+            $rating->rateable_type = $rating->product_id;
+            $rating->rateable_id = \App\Product::class;
+            $rating->save();
+        });
+
+        Schema::table('ratings', function (Blueprint $table) {
+            $table->dropColumn(['user_id', 'product_id']);
+        });
     }
 
     public function down()
     {
         Schema::table('ratings', function (Blueprint $table) {
             $table->dropColumn(['rater_type', 'rater_id', 'rateable_type', 'rateable_id']);
+            $table->unsignedBigInteger('product_id');
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 }
