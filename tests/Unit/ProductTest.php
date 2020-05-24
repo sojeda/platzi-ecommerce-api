2672ce5abbcdb6c6f50fe88e254bf4bf4cfd9979
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 12:10:29 -0300

    Reto Clase 6 - Agregar relacion de Producto-Usuario

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Category;
 use App\Product;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -17,5 +18,15 @@ class ProductTest extends TestCase
         $product = factory(Product::class)->create(['category_id' => $category->id]);
 
         $this->assertInstanceOf(Category::class, $product->category);
+        $this->assertEquals($category->id, $product->category->id);
+    }
+
+    public function test_a_product_belongs_to_user()
+    {
+        $user = factory(User::class)->create();
+        $product = factory(Product::class)->create(['created_by' => $user->id]);
+
+        $this->assertInstanceOf(User::class, $product->createdBy);
+        $this->assertEquals($user->id, $product->createdBy->id);
     }
 }
