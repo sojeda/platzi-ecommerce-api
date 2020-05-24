019fafa3722b19f29d2597b18713a383de64039e
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 14:37:58 -0300

    Reto Clase 7

--- tests/Unit/RatingTest.php
+++ tests/Unit/RatingTest.php
@@ -39,23 +39,36 @@ class RatingTest extends TestCase
 
         $user->rate($product, 5);
         $user2->rate($product, 10);
+        $user->rate($user, 5);
 
         $this->assertEquals(7.5, $product->averageRating(User::class));
+        $this->assertEquals(5, $user->averageRating(User::class));
     }
 
     public function test_rating_model()
     {
         /** @var User $user */
         $user = factory(User::class)->create();
+        /** @var User $user2 */
+        $user2 = factory(User::class)->create();
         /** @var Product $product */
         $product = factory(Product::class)->create();
 
         $user->rate($product, 5);
+        $user->rate($user2, 3);
 
         /** @var Rating $rating */
         $rating = Rating::first();
+        $rating2 = Rating::get()->last();
 
         $this->assertInstanceOf(Product::class, $rating->rateable);
         $this->assertInstanceOf(User::class, $rating->qualifier);
+        $this->assertEquals($user->id, $rating->qualifier->id);
+        $this->assertEquals($product->id, $rating->rateable->id);
+
+        $this->assertInstanceOf(User::class, $rating2->rateable);
+        $this->assertInstanceOf(User::class, $rating2->qualifier);
+        $this->assertEquals($user->id, $rating2->qualifier->id);
+        $this->assertEquals($user2->id, $rating2->rateable->id);
     }
 }
