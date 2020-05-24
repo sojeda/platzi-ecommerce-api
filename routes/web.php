e88bc03a8c4418d58083816687d4b599dcf1aef6
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 15:49:57 -0300

    Clase 8 - Habilitar verificacion de correo

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
