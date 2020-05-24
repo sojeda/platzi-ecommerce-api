45b263a482d25d3f2981b0a040e35068d8972adb
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 20:04:57 -0300

    Clase 13 - Envio de correo en el Job

--- app/Jobs/SendWelcomeEmail.php
+++ app/Jobs/SendWelcomeEmail.php
@@ -2,23 +2,29 @@
 
 namespace App\Jobs;
 
+use App\Mail\WelcomeEmail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 
 class SendWelcomeEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct()
+    private string $userEmail;
+
+    public function __construct(string $userEmail)
     {
-        //
+        $this->userEmail = $userEmail;
     }
 
     public function handle()
     {
-        //
+        $email = new WelcomeEmail();
+
+        Mail::to($this->userEmail)->send($email);
     }
 }
