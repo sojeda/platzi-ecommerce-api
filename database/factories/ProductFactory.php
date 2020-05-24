f9ed91f6cb55b699dc3d828cf2c9663041780ed4
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 01:11:04 -0300

    Calculo de Promedio

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,12 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Category;
 use Faker\Generator as Faker;
 use App\Product;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'price' => $faker->numberBetween(10000, 60000)
+        'price' => $faker->numberBetween(10000, 60000),
+        'category_id' => function (array $post) {
+            return Category::inRandomOrder()->first()->id;
+        },
     ];
 });
