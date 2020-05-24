893923c5cb768e17a0bb68b3939b3ea24688083d
Sergio Ojeda <soj3da@gmail.com>
2020-05-23 23:43:45 -0300

    Clase 6 - Relacion entre Categoria y Producto

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $guarded = [];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
