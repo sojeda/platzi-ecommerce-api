94c32aaaabaaca528b6987f184534f84ffba6fb1
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 15:49:57 -0300

    Clase 8 - Habilitar verificacion de correo

--- app/User.php
+++ app/User.php
@@ -4,11 +4,12 @@ namespace App;
 
 use App\Utils\CanBeRate;
 use App\Utils\CanRate;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable, HasApiTokens, CanRate, CanBeRate;
 
