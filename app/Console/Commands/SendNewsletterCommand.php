a39e89aec7edb370367600bc2ce0447f799a8d50
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 16:20:51 -0300

    Agregar argumentos, y preguntas.

--- app/Console/Commands/SendNewsletterCommand.php
+++ app/Console/Commands/SendNewsletterCommand.php
@@ -8,15 +8,34 @@ use Illuminate\Console\Command;
 
 class SendNewsletterCommand extends Command
 {
-    protected $signature = 'send:newsletter';
+    protected $signature = 'send:newsletter {emails?*}';
 
     protected $description = 'Envia un correo electronico a todos los usuarios que hayan verificado su cuenta';
 
     public function handle()
     {
-        User::query()->whereNotNull('email_verified_at')
-            ->each(function (User $user) {
-                $user->notify(new NewsletterNotification());
-            });
+        $userEmails = $this->argument('emails');
+
+        $builder = User::query();
+
+        if ($userEmails) {
+            $builder->whereIn('email', $userEmails);
+        }
+
+        $builder->whereNotNull('email_verified_at');
+
+        if ($count = $builder->count()) {
+            $this->info("Se enviaran {$count} correos");
+
+            if ($this->confirm('¿Estas de acuerdo?')) {
+                $builder->each(function (User $user) {
+                    $user->notify(new NewsletterNotification());
+                });
+
+                return;
+            }
+        }
+
+        $this->info('No se enviaron correos');
     }
 }
