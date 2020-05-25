3d6bef831ddcb6ef4bd6191cea9d946a251ccc74
Sergio Ojeda <soj3da@gmail.com>
2020-05-24 23:47:37 -0300

    Clase 19 - Test

--- tests/Feature/ProductRatingControllerTest.php
+++ tests/Feature/ProductRatingControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Product;
+use App\Rating;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Event;
@@ -24,7 +25,7 @@ class ProductRatingControllerTest extends TestCase
         parent::setUp();
         Event::fake();
         $this->normalUser = factory(User::class)->create();
-        $this->adminUser = factory(User::class)->create();
+        $this->adminUser = factory(User::class)->state('admin')->create();
         $this->product = factory(Product::class)->create();
     }
 
@@ -103,5 +104,58 @@ class ProductRatingControllerTest extends TestCase
         $response->assertJson([
             'error' => "EL valor debe estar entre 1 y 5"
         ]);
+    }
+
+    public function test_approve_rating_with_admin()
+    {
+        Sanctum::actingAs($this->adminUser);
+
+        $this->normalUser->rate($this->product, 3);
+
+        $rating = Rating::first();
+
+        $response = $this->postJson("/api/rating/{$rating->getKey()}/approve");
+
+        $response->assertSuccessful();
+
+        $rating->refresh();
+
+        $this->assertNotNull($rating->approved_at);
+    }
+
+    public function test_approve_rating_without_admin()
+    {
+        Sanctum::actingAs($this->normalUser);
+
+        $this->normalUser->rate($this->product, 3);
+
+        $rating = Rating::first();
+
+        $response = $this->postJson("/api/rating/{$rating->getKey()}/approve");
+
+        $response->assertStatus(403);
+    }
+
+    public function test_index_rating()
+    {
+        Sanctum::actingAs($this->adminUser);
+
+        $this->normalUser->rate($this->product, 3);
+        $this->adminUser->rate($this->product, 3);
+
+        $response = $this->getJson("/api/rating");
+
+        $response->assertOk();
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonCount(2, 'data');
+    }
+
+    public function test_index_rating_with_normal_user()
+    {
+        Sanctum::actingAs($this->normalUser);
+
+        $response = $this->getJson("/api/rating");
+
+        $response->assertStatus(403);
     }
 }
