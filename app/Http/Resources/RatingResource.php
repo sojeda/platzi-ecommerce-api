7c80b2960f8c4cf8072dbaf2005f0aa9e634c79f
Sergio Ojeda <soj3da@gmail.com>
2020-05-25 21:53:52 -0300

    Clase 19 - Test

--- app/Http/Resources/RatingResource.php
+++ app/Http/Resources/RatingResource.php
@@ -2,21 +2,21 @@
 
 namespace App\Http\Resources;
 
+use App\Rating;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class RatingResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
-     *
      * @param \Illuminate\Http\Request $request
      * @return array
+     * @see Rating
      */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'rate' => $this->rate,
+            'score' => $this->score,
             'comments' => $this->comments,
             'rateable' => $this->rateable,
             'qualifier' => $this->qualifier,
