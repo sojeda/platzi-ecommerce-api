4f837b504b24b6bc2ca70f8d55c5e39926c49b76
Sergio Ojeda <soj3da@gmail.com>
2020-05-26 08:55:19 -0300

    Remove And Update Migration

--- database/migrations/2020_05_24_151259_update_rating_table.php
+++ database/migrations/2020_05_24_151259_update_rating_table.php
@@ -8,7 +8,11 @@ class UpdateRatingTable extends Migration
 {
     public function up()
     {
-        Schema::table('ratings', function (Blueprint $table) {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->float('score');
+            $table->timestamps();
+
             /*
             Equivalente a
             $table->unsignedBigInteger('rateable_id');
@@ -17,38 +21,13 @@ class UpdateRatingTable extends Migration
             $table->integer('qualifier_id');
             $table->string('qualifier_type');
              */
-            // Sql Lite Testing
             $table->nullableMorphs('rateable');
             $table->nullableMorphs('qualifier');
-        });
-
-        \App\Rating::all()->each(function (\App\Rating $rating) {
-            $rating->qualifier_type = \App\User::class;
-            $rating->qualifier_id = $rating->user_id;
-            $rating->rateable_type = $rating->product_id;
-            $rating->rateable_id = \App\Product::class;
-            $rating->save();
-        });
-
-        Schema::table('ratings', function (Blueprint $table) {
-            if (env('DB_CONNECTION') !== 'sqlite') {
-                // Not working on SQL Lite
-                $table->dropForeign('ratings_product_id_foreign');
-                $table->dropForeign('ratings_user_id_foreign');
-            }
-            $table->dropColumn(['user_id', 'product_id']);
         });
     }
 
     public function down()
     {
-        Schema::table('ratings', function (Blueprint $table) {
-            $table->dropColumn(['qualifier_type', 'qualifier_id', 'rateable_type', 'rateable_id']);
-            $table->unsignedBigInteger('product_id');
-            $table->unsignedBigInteger('user_id');
-
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->foreign('user_id')->references('id')->on('users');
-        });
+        Schema::dropIfExists('ratings');
     }
 }
